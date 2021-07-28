4f307c689a08d3ce88b2d8612cee8f16ba52fc15
zhangxu1161 <zhangxu1161@gmail.com>
2021-07-28 17:18:08 +0800

    Add baseFee query

--- src/services/crust/api.ts
+++ src/services/crust/api.ts
@@ -2,6 +2,7 @@ import { ApiPromise, WsProvider } from '@polkadot/api'
 import { typesBundleForPolkadot } from '@crustio/type-definitions'
 
 const chainWsUrl = process.env.NODE_ENV === 'production' ? process.env.WS_ENDPOINT : 'wss://rocky-api.crust.network'
+// wss://api-maxwell.crust.network
 
 export const api = new ApiPromise({
   provider: new WsProvider(chainWsUrl),
